0ac736d26b4dae1f3a7de1c3d0233e129be76e9a
Aurelijus Banelis <aurelijus@banelis.lt>
2019-03-30 15:39:25 +0200

    Upgrade: add bootstrap dependencies
    Bootstrap is a common CSS framework to make beautiful Websites fast and easy.
    Using yarn to store frontend libraries in node_modules/ folder instead of pushing all code to GitHub.
    
    Updated assets by example in Symfony documentation.
    
    Code generated with:
      yarn add bootstrap --dev
      yarn add jquery popper.js --dev
    
    Documentation:
      https://symfony.com/doc/current/frontend/encore/bootstrap.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
