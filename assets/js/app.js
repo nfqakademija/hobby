00604d6df72b76ba828b9ecdc435c00a6ed46394
Aurelijus Banelis <aurelijus@banelis.lt>
2019-03-30 16:06:46 +0200

    Upgrade: Wire HTML, JavaScript and CSS
    With new Encore/Webpack, CSS and JavaScript are processed together: CSS is included only if it is used in JavaScript file.
    
    Documentation:
      https://symfony.com/doc/current/frontend/encore/simple-example.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ const $ = require('jquery');
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
+require('../css/app.scss');
+
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
