db78a10e3a91ab67b043cd14f9a92b6dc97de36f
Lukas <lukas.zmoginas@gmail.com>
2019-04-26 02:05:14 +0300

    task-8rtidi add fixtures for hobby

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/DataFixtures/HobbyData.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Hobby;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class HobbyData extends Fixture
+{
+    /**
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $hobby = new Hobby();
+        $titles = ['Tenisas', 'Kartingai', 'Krepsinis'];
+        $descriptions = ['Smagus', 'Nuobodus', 'Itraukiantis'];
+        $usernames = ['Ieva', 'Viktoras', 'Miroslav'];
+        $email = ['istanynaite@nfq.lt', 'vkarcikovas@gmail.com', 'mpetkevic@gmail.com'];
+        $amounts = [100, 99.99, 1.12];
+        $endDates = ['2018-09-10', '2019-01-01', '2017-07-07'];
+
+        for ($i = 0; $i < 3; $i++) {
+            $hobby->setTitle($titles[array_rand($titles)]);
+            $hobby->setDescription($descriptions[array_rand($descriptions)]);
+            $hobby->setUsername($usernames[array_rand($usernames)]);
+            $hobby->setEmail($email[array_rand($email)]);
+            $hobby->setAmount($amounts[array_rand($amounts)]);
+            $hobby->setEndDate($endDates[array_rand($endDates)]);
+
+            $manager->persist($hobby);
+            $manager->flush();
+            $manager->clear();
+        }
+    }
+}
