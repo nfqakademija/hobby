1c836f267f2c5a065c054a75bc3a914438cca1fb
Lukas <lukas.zmoginas@gmail.com>
2019-04-26 02:20:45 +0300

    task-aizwch composer require lexik/jwt-authentication-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
