ca6695c7f1f5ad614f34ed073cbc379597f64eb6
Victor Karcikovas <random_thoughts_vick@Victors-MacBook-Pro.local>
2019-05-05 00:29:59 +0300

    added material-ui

--- assets/js/components/Home/Home.js
+++ assets/js/components/Home/Home.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link as RouterLink } from "react-router-dom";
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 import './Home.scss';
 
-const Home = () => {
+const Home = ( ) => {
   return (
     <div className='Home'>
-      <p>This is a place for your hobby projects.</p>
-      <p>To see projects, please go to{' '}
-        <Link className='Link' to='/projects'>Projects List</Link>
-      </p>
-      <p>If you have an account please{' '}
-        <Link className='Link' to='/login'>Sing In</Link>
-      </p>
-      <p>If you don't have an account please{' '}
-        <Link className='Link' to='/register'>Sing Up</Link>
-      </p>
+        <Paper elevation={2}>
+            <Typography variant="h5" component="h3">This is a place for your hobby projects.</Typography>
+          <Typography variant='subtitle1'  >To see projects, please go to{' '}
+            <Link component={RouterLink} color="$link-color"  to='/projects'>Projects List</Link>
+          </Typography>
+          <Typography variant='subtitle1'>If you have an account please{' '}
+            <Link component={RouterLink}  className='Link' to='/login'>Sing In</Link>
+          </Typography>
+          <Typography variant='subtitle1'>If you don't have an account please{' '}
+            <Link component={RouterLink} className='Link' to='/register'>Sing Up</Link>
+          </Typography>
+        </Paper>
     </div>
   );
 };
--- assets/js/components/NavBar/NavBar.js
+++ assets/js/components/NavBar/NavBar.js
@@ -16,65 +16,66 @@ class NavBar extends Component {
     const {isActive} = this.state;
     return (
       <div className='NavBar'>
-        <Link className='Logo-link' to='/'><Logo/></Link>
-        <nav className='Navigation'>
-          <ul>
-            <NavLink exact to='/' className='NavBar-link' activeClassName='active-link'>Home</NavLink>
-            <NavLink to='/projects' className='NavBar-link' activeClassName='active-link'>Projects</NavLink>
-            <NavLink to='/project-registration' className='NavBar-link New-Project' activeClassName='active-link'>New Project</NavLink>
-          </ul>
-        </nav>
 
-        <div className='SignIn-links'>
-          <Link className='SignIn-link' to='/login'>Sign In</Link>
-          <Link className='SignIn-link' to='/register'>Sign Up</Link>
-        </div>
-        <button
-          onClick={this.toggleActive}
-          className={isActive ?
-            'hamburger hamburger--squeeze is-active' : 'hamburger hamburger--squeeze'}
-          type="button">
-            <span className="hamburger-box">
-              <span className="hamburger-inner"></span>
-            </span>
-        </button>
-        {isActive ?
-          <div className='Hover'>
-            <nav className='Hover--Navigation'>
+            <Link className='Logo-link' to='../'><Logo src='../Test.png'/></Link>
+            <nav className='Navigation'>
               <ul>
-                <NavLink
-                  exact
-                  to='/'
-                  className='NavBar-link'
-                  activeClassName='active-link'
-                  onClick={this.toggleActive}
-                >Home</NavLink>
-                <NavLink
-                  to='/projects'
-                  className='NavBar-link'
-                  activeClassName='active-link'
-                  onClick={this.toggleActive}
-                >Projects</NavLink>
-                <NavLink
-                  to='/project-registration'
-                  className='NavBar-link New-Project'
-                  activeClassName='active-link'
-                  onClick={this.toggleActive}
-                >New Project</NavLink>
+                <NavLink exact to='/' className='NavBar-link' activeClassName='active-link'>Home</NavLink>
+                <NavLink to='/projects' className='NavBar-link' activeClassName='active-link'>Projects</NavLink>
+                <NavLink to='/project-registration' className='NavBar-link New-Project' activeClassName='active-link'>New Project</NavLink>
               </ul>
             </nav>
-            <div className='Hover--SignIn-links'>
-              <Link
-                onClick={this.toggleActive}
-                className='SignIn-link'
-                to='/login'>Sign In</Link>
-              <Link
-                onClick={this.toggleActive}
-                className='SignIn-link'
-                to='/register'>Sign Up</Link>
+
+            <div className='SignIn-links'>
+              <Link className='SignIn-link' to='/login'>Sign In</Link>
+              <Link className='SignIn-link' to='/register'>Sign Up</Link>
             </div>
-          </div> : null
-        }
+            <button
+              onClick={this.toggleActive}
+              className={isActive ?
+                'hamburger hamburger--squeeze is-active' : 'hamburger hamburger--squeeze'}
+              type="button">
+                <span className="hamburger-box">
+                  <span className="hamburger-inner"></span>
+                </span>
+            </button>
+            {isActive ?
+              <div className='Hover'>
+                <nav className='Hover--Navigation'>
+                  <ul>
+                    <NavLink
+                      exact
+                      to='/'
+                      className='NavBar-link'
+                      activeClassName='active-link'
+                      onClick={this.toggleActive}
+                    >Home</NavLink>
+                    <NavLink
+                      to='/projects'
+                      className='NavBar-link'
+                      activeClassName='active-link'
+                      onClick={this.toggleActive}
+                    >Projects</NavLink>
+                    <NavLink
+                      to='/project-registration'
+                      className='NavBar-link New-Project'
+                      activeClassName='active-link'
+                      onClick={this.toggleActive}
+                    >New Project</NavLink>
+                  </ul>
+                </nav>
+                <div className='Hover--SignIn-links'>
+                  <Link
+                    onClick={this.toggleActive}
+                    className='SignIn-link'
+                    to='/login'>Sign In</Link>
+                  <Link
+                    onClick={this.toggleActive}
+                    className='SignIn-link'
+                    to='/register'>Sign Up</Link>
+                </div>
+              </div> : null
+            }
       </div>
     );
   }
