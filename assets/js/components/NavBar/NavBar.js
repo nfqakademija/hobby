dbd692ac9347c6db8e9ab3dcf0e8b95efcfb6a8e
Viktoras Karcikovas <45361674+Karcikovas@users.noreply.github.com>
2019-05-12 23:34:17 +0300

    Added more styling for NavBar

--- assets/js/components/NavBar/NavBar.js
+++ assets/js/components/NavBar/NavBar.js
@@ -5,12 +5,15 @@ import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import {onLogOut as Logout} from '../../thunks/logoutThunk';
-
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Typography from '@material-ui/core/Typography'
 
 const theme = createMuiTheme({
     palette: {
@@ -25,54 +28,106 @@ const theme = createMuiTheme({
 
 const styles = {
     root: {
+        width: '100%',
+    },
+    grow: {
         flexGrow: 1,
     },
-    Toolbar:{
-      height: 150,
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
     },
-    Tabs:{
-    width: '100%',
-    }
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
 
 };
 class NavBar extends Component {
   state ={
       value: 0,
+      anchorEl: null,
   }
 
     handleChange = (event, value) => {
         this.setState({ value });
     };
 
+
+    handleProfileMenuOpen = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleMenuClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+
    render() {
       const { classes, onLogout } = this.props;
-      const {email, isAuth, amount} = this.props.auth;
+      const { anchorEl } = this.state;
+      const { isAuth } = this.props.auth;
+      const isMenuOpen = Boolean(anchorEl)
+
+       const renderMenu = (
+           <Menu
+               anchorEl={anchorEl}
+               anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+               transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+               open={isMenuOpen}
+               onClose={this.handleMenuClose}
+           >
+               <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+               <MenuItem
+               //     onClick={() => {
+               //     handleMenuClose();
+               //     anotherfunctionCall();
+               // }}
+                   onClick={this.handleMenuClose}
+                         component={RouterLink} to='/logout'>Logout</MenuItem>
+
+
+           </Menu>
+       );
+
+
       return (
           <MuiThemeProvider theme={theme}>
-      <Grid container className={classes.root} >
-            <AppBar   color="default" elevation={8} >
-                <Toolbar className={classes.height} >
-                    <Tabs  className={classes.Tabs}
-                           value={this.state.value}
-                           onChange={this.handleChange}
-                           componentColor="primary"
-                           indicatorColor="primary"
-                           textColor="primary"
-                           centered
-                    >
-                          <Tab   component={RouterLinkNav} exact to='/' label="Home" />
-                          <Tab  component={RouterLinkNav} to='/projects'   label="Projects" />
-                          <Tab  component={RouterLinkNav} to='/project-registration' label="Create Project" />
-                        {isAuth ? <Tab  component={RouterLink}  to='/user' label={`${email} (${amount}€)`} /> :
-                            <Tab  component={RouterLink}  to='/login' label="Sign In" />
+      <div className={classes.root} >
+            <AppBar  position="static" color="default" >
+                <Toolbar >
+                    <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+                        <MenuIcon />
+                    </IconButton>
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
+                        <Button  component={RouterLinkNav} exact to='/'>Home</Button>
+                        <Button  component={RouterLinkNav} to='/projects'>Projects</Button>
+                        <Button  component={RouterLinkNav} to='/project-registration'>Create Project</Button>
+
+                        {isAuth ? null :
+                            <Button color='primary' variant="contained" component={RouterLink}  to='/register'>Sign Up</Button>
                         }
-                        {isAuth ? <Tab onClick={onLogout}  component={RouterLink}  to='/logout' label='Logout' /> :
-                            <Tab  component={RouterLink}  to='/register' label="Sign Up" />
+                        {isAuth ? <Button disabled  component={RouterLink}>{this.props.auth.email}  {this.props.auth.amount}€</Button>
+                            :
+                            <Button color='primary' variant="contained" component={RouterLink}  to='/login'>Sign In</Button>
                         }
-                    </Tabs>
+                        {isAuth ? <IconButton
+                                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+                                aria-haspopup="true"
+                                onClick={this.handleProfileMenuOpen}
+                                color="inherit"
+                            > <AccountCircle/> </IconButton>
+                            :
+                            null
+                        }
+                    </div>
                 </Toolbar>
             </AppBar>
-      </Grid>
+          {renderMenu}
+      </div>
           </MuiThemeProvider>
     );
   }
