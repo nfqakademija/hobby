a4adec0e3701c4c50b2e749e2d711d340e8bcb64
Viktoras Karcikovas <random_thoughts_vick@Victors-MacBook-Pro.local>
2019-05-18 16:49:23 +0300

    Task-7656c0 changed NavBar Style

--- assets/js/components/NavBar/NavBar.js
+++ assets/js/components/NavBar/NavBar.js
@@ -7,20 +7,13 @@ import Toolbar from '@material-ui/core/Toolbar';
 import { withStyles } from '@material-ui/core/styles';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import {onLogOut as Logout} from '../../thunks/logoutThunk';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import Typography from '@material-ui/core/Typography'
 
 const theme = createMuiTheme({
     palette: {
         primary: {
             main: '#EA7925',
-        },
-        secondary: {
-            main: '#ffffff',
         },
     },
 });
@@ -29,117 +22,93 @@ const styles = {
     root: {
         width: '100%',
     },
+    menubar: {
+        paddingLeft: 100,
+        paddingRight: 100,
+        position:"static",
+        background:"#181818",
+
+    },
     grow: {
         flexGrow: 1,
+        align: 'center',
     },
-    menuButton: {
-        marginRight: 100,
+    logo: {
+        color: '#ffffff',
     },
-    menuButtons:{
-        marginLeft: 100,
-    },
-
-    menuButtonRight: {
-        marginRight: 5,
-    },
-    sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'flex',
+    button: {
+        color: '#A4A4A4',
+        '&:hover': {
+            color: '#ffffff',
         },
+        margin: 'auto',
+        paddingLeft:  20,
+        paddingRight: 20,
     },
+    buttonSingUp: {
+        color: '#ffffff',
+        backgroundColor: "#EA7925",
+        borderRadius: 25,
+        fontWeight: 600,
+        '&:hover': {
+            backgroundColor: "#A15421",
+        },
+    }
 
 };
 class NavBar extends Component {
   state ={
       value: 0,
-      anchorEl: null,
   }
 
     handleChange = (event, value) => {
         this.setState({ value });
     };
 
-
-    handleProfileMenuOpen = event => {
-        this.setState({ anchorEl: event.currentTarget });
-    };
-
-    handleMenuClose = () => {
-        this.setState({ anchorEl: null });
-    };
-
-
    render() {
       const { classes } = this.props;
-      const { anchorEl } = this.state;
       const { isAuth } = this.props.auth;
-      const isMenuOpen = Boolean(anchorEl)
-
-
-       const renderMenu = (
-           <Menu
-               anchorEl={anchorEl}
-               anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-               transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-               open={isMenuOpen}
-               onClose={this.handleMenuClose}
-           >
-               <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
-               <MenuItem onClick={this.props.auth.onLogout} component={RouterLink} to='/logout'>Logout</MenuItem>
-           </Menu>
-       );
-
 
       return (
           <MuiThemeProvider theme={theme}>
-      <div className={classes.root} >
-            <AppBar position="static" color="default">
-                <Toolbar>
-                    <div className={classes.sectionDesktop}>
-                        <div className={classes.menuButtons}>
-                        {isAuth ? <Button  component={RouterLinkNav} exact to='/'>Home</Button> :
-                           null
-                        }
-
-                        {isAuth ? <Button  component={RouterLinkNav} to='/projects'>Projects</Button> :
-                            null
-                        }
-                        {isAuth ? <Button  component={RouterLinkNav} to='/project-registration'>Create Project</Button> :
-                            null
-                        }
-                        </div>
-                    </div>
-                    <div className={classes.grow} />
-                    <div className={classes.sectionDesktop}>
-                        {isAuth ? <Button color='default' disabled >{this.props.auth.email}  {this.props.auth.amount}€</Button>
-                            :
-                            null
-                        }
-                        {isAuth ? <IconButton
-                                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
-                                aria-haspopup="true"
-                                onClick={this.handleProfileMenuOpen}
-                                color="inherit"
-                            > <AccountCircle/> </IconButton>
-                            :
-                            null
-                        }
-                        <div className={classes.menuButton} >
-                        {isAuth ? null :
-                            <Button className={classes.menuButtonRight} textColor="secondary" color='primary' variant="contained" component={RouterLink}  to='/register'>
-                                <Typography color="secondary" >Sign Up</Typography></Button>
-                        }
-                        {isAuth ? null :
-                            <Button  color='primary' variant="contained" component={RouterLink}  to='/login'>
-                                <Typography color="secondary" >Sign In</Typography></Button>
-                        }
-                        </div>
-                    </div>
-                </Toolbar>
-            </AppBar>
-          {renderMenu}
-      </div>
+            <div className={classes.root} >
+                <AppBar className={classes.menubar} >
+                    {isAuth ?
+                    <Toolbar>
+                        <Typography variant="h6" className={classes.logo}>
+                            HobbyCraft
+                        </Typography>
+                        <div className={classes.grow} />
+                        <Button className={classes.button} component={RouterLinkNav} exact to='/'>
+                            Discover
+                        </Button>
+                        <Button className={classes.buttonSingUp} component={RouterLink}  to='/register'>Sign Up With Email</Button>
+                        <Button className={classes.button}>
+                            About Us
+                        </Button>
+                        <div className={classes.grow} />
+                        <Button className={classes.button} component={RouterLink}  to='/login'>
+                            Sign In
+                        </Button>
+                    </Toolbar> : <Toolbar>
+                            <Typography variant="h6" className={classes.logo}>
+                                HobbyCraft
+                            </Typography>
+                            <div className={classes.grow} />
+                            <Button className={classes.button} component={RouterLinkNav} to='/projects'>
+                                Projects
+                            </Button>
+                            <Button className={classes.buttonSingUp} component={RouterLinkNav} to='/project-registration'>Create Project</Button>
+                            <Button className={classes.button}>
+                                About Us
+                            </Button>
+                            <div className={classes.grow} />
+                            <Button className={classes.button} onClick={this.props.auth.onLogout} component={RouterLink} to='/logout'>
+                                Logout
+                            </Button>
+                        </Toolbar>}
+                </AppBar>
+            </div>
           </MuiThemeProvider>
     );
   }
