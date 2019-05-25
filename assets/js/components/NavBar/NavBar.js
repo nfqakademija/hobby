e0915a1e0545ba50f17b49588d499024c4c7f8b0
mpetkevic <miroslav.petkevic@gmail.com>
2019-05-25 11:59:03 +0300

    Logout Fix

--- assets/js/components/NavBar/NavBar.js
+++ assets/js/components/NavBar/NavBar.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { NavLink as RouterLinkNav , Link as RouterLink } from 'react-router-dom';
+import React, {Component} from 'react';
+import {NavLink as RouterLinkNav, Link as RouterLink, withRouter} from 'react-router-dom';
 import {connect} from "react-redux";
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { withStyles } from '@material-ui/core/styles';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import {onLogOut as Logout} from '../../thunks/logoutThunk';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -14,171 +14,173 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#EA7925',
-        },
+  palette: {
+    primary: {
+      main: '#EA7925',
     },
+  },
 });
 
 const styles = {
-    root: {
-        width: '100%',
-    },
-    menubar: {
-        position:"static",
-        background:"#181818",
+  root: {
+    width: '100%',
+  },
+  menubar: {
+    position: "static",
+    background: "#181818",
 
+  },
+  grow: {
+    flexGrow: 1,
+    align: 'center',
+  },
+  title: {
+    color: '#ffffff',
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
     },
-    grow: {
-        flexGrow: 1,
-        align: 'center',
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
     },
-    title: {
-        color: '#ffffff',
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'block',
-        },
+  },
+  button: {
+    color: '#A4A4A4',
+    '&:hover': {
+      color: '#ffffff',
     },
-    sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'block',
-        },
+    margin: 'auto',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  buttonSingUp: {
+    color: '#ffffff',
+    backgroundColor: "#EA7925",
+    borderRadius: 25,
+    fontWeight: 600,
+    '&:hover': {
+      backgroundColor: "#A15421",
     },
-    button: {
-        color: '#A4A4A4',
-        '&:hover': {
-            color: '#ffffff',
-        },
-        margin: 'auto',
-        paddingLeft:  20,
-        paddingRight: 20,
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
     },
-    buttonSingUp: {
-        color: '#ffffff',
-        backgroundColor: "#EA7925",
-        borderRadius: 25,
-        fontWeight: 600,
-        '&:hover': {
-            backgroundColor: "#A15421",
-        },
-    },
-    sectionMobile: {
-        display: 'flex',
-        [theme.breakpoints.up('md')]: {
-            display: 'none',
-        },
-    },
-    titleMobile: {
-        color: '#ffffff',
-    },
-    IconMobile: {
-        color:"#ffffff",
-    },
+  },
+  titleMobile: {
+    color: '#ffffff',
+  },
+  IconMobile: {
+    color: "#ffffff",
+  },
 };
 
 class NavBar extends Component {
-  state ={
-      value: 0,
+  state = {
+    value: 0,
   }
 
-    handleChange = (event, value) => {
-        this.setState({ value });
-    };
+  handleChange = (event, value) => {
+    this.setState({value});
+  };
 
+  onLogOutToggle = () => this.props.onLogoutClick(this.props.history)
 
+  render() {
+    const {classes } = this.props;
+    const {isAuth} = this.props.auth;
 
-   render() {
-      const { classes } = this.props;
-      const { isAuth } = this.props.auth;
+    return (
+        <MuiThemeProvider theme={theme}>
+          <div className={classes.root}>
+            <AppBar className={classes.menubar}>
+              <Toolbar>
+                <Typography variant="h6" className={classes.title}>
+                  HobbyCraft
+                </Typography>
 
-      return (
-          <MuiThemeProvider theme={theme}>
-            <div className={classes.root} >
-                <AppBar className={classes.menubar} >
-                        <Toolbar>
-                            <Typography variant="h6" className={classes.title}>
-                                HobbyCraft
-                            </Typography>
+                <div className={classes.sectionMobile}>
+                  <IconButton className={classes.IconMobile}>
+                    <MenuIcon/>
+                  </IconButton>
+                </div>
 
-                            <div className={classes.sectionMobile}>
-                                <IconButton className={classes.IconMobile}>
-                                    <MenuIcon/>
-                                </IconButton>
-                            </div>
+                <div className={classes.grow}/>
+                <div className={classes.sectionDesktop}>
+                  {isAuth ?
+                      < Button className={classes.button} component={RouterLinkNav} exact to='/projects'>
+                        Discover Projects
+                      </Button>
+                      :
+                      < Button className={classes.button} component={RouterLinkNav} exact to='/'>
+                        Discover
+                      </Button>}
+                  {isAuth ?
+                      <Button className={classes.buttonSingUp} component={RouterLinkNav} to='/project-registration'>Create
+                        a Project</Button>
+                      :
+                      <Button className={classes.buttonSingUp} component={RouterLinkNav} to='/register'>Sign
+                        Up With Email</Button>
+                  }
+                  {isAuth ?
+                      <Button className={classes.button} component={RouterLinkNav}
+                              to='/user'>{this.props.auth.email} {this.props.auth.amount}€</Button>
+                      :
+                      <Button className={classes.button}>
+                        About Us
+                      </Button>
+                  }
+                </div>
+                <div className={classes.sectionMobile}>
+                  <Typography variant="h6" className={classes.titleMobile}>
+                    HobbyCraft
+                  </Typography>
+                </div>
 
-                            <div className={classes.grow}  />
-                            <div className={classes.sectionDesktop} >
-                                {isAuth ?
-                                    < Button className={classes.button} component={RouterLinkNav} exact to='/projects'>
-                                        Discover Projects
-                                    </Button>
-                                    :
-                                    < Button className={classes.button} component={RouterLinkNav} exact to='/'>
-                                        Discover
-                                    </Button> }
-                                {isAuth ?
-                                    <Button className={classes.buttonSingUp} component={RouterLinkNav} to='/project-registration'>Create a Project</Button>
-                                    :
-                                    <Button className={classes.buttonSingUp} component={RouterLinkNav} to='/register'>Sign
-                                        Up With Email</Button>
-                                }
-                                {isAuth ?
-                                    <Button className={classes.button} component={RouterLinkNav} to='/user'>{this.props.auth.email}  {this.props.auth.amount}€</Button>
-                                    :
-                                    <Button className={classes.button}>
-                                        About Us
-                                    </Button>
-                                }
-                            </div>
-                            <div className={classes.sectionMobile}>
-                                <Typography variant="h6" className={classes.titleMobile}>
-                                    HobbyCraft
-                                </Typography>
-                            </div>
+                <div className={classes.grow}/>
+                <div className={classes.sectionDesktop}>
+                  {isAuth ?
+                      < Button className={classes.button} onClick={this.onLogOutToggle} to='/logout'>
+                        Logout
+                      </Button>
+                      :
+                      < Button className={classes.button} component={RouterLink} to='/login'>
+                        Sign In
+                      </Button>
+                  }
+                </div>
 
-                            <div className={classes.grow} />
-                            <div className={classes.sectionDesktop}>
-                                {isAuth  ?
-                                    < Button className = {classes.button} onClick={this.props.onLogoutClick} to='/logout'>
-                                    Logout
-                                    </Button>
-                                    :
-                                    < Button className = {classes.button} component={RouterLink}  to='/login'>
-                                        Sign In
-                                    </Button>
-                                }
-                            </div>
+                <div className={classes.sectionMobile}>
+                  <IconButton className={classes.IconMobile}>
+                    <SearchIcon/>
+                  </IconButton>
+                </div>
 
-                            <div className={classes.sectionMobile}>
-                                <IconButton className={classes.IconMobile}>
-                                    <SearchIcon />
-                                </IconButton>
-                            </div>
-
-                        </Toolbar>
-                </AppBar>
-            </div>
-          </MuiThemeProvider>
+              </Toolbar>
+            </AppBar>
+          </div>
+        </MuiThemeProvider>
     );
   }
 }
 
 NavBar.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 
 const mapStateToProps = (state) => {
-    return {
-        auth: state.auth
-    }
+  return {
+    auth: state.auth
+  }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    onLogoutClick: () => dispatch(Logout())
+  onLogoutClick: (history) => dispatch(Logout(history))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(NavBar));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(withStyles(styles)(NavBar)));
--- assets/js/thunks/logoutThunk.js
+++ assets/js/thunks/logoutThunk.js
@@ -2,13 +2,13 @@ import * as actions from '../actions/authActions';
 import {removeUserFromLS} from '../storage/storage'
 import axios from 'axios';
 
-export const onLogOut = () => (dispatch) => {
-    console.log('LOGOUT')
-    axios.get('/api/security/logout')
-        .then(res=> {
-            removeUserFromLS()
-            dispatch(actions.logout())
-        })
-        .catch(err => console.log(err))
-    return null;
+export const onLogOut = (history) => (dispatch) => {
+  axios.get('/api/security/logout')
+      .then(res => {
+        removeUserFromLS()
+        dispatch(actions.logout())
+        history.push('/');
+      })
+      .catch(err => console.log(err))
+  return null;
 }
